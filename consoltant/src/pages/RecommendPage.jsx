d62e4f8012ef27097bc133885826d54c1ee45754
dahyunko <kdhgby@naver.com>
2024-08-18 18:25:04 +0900

    feat: tailwind 환경설정

--- consoltant/src/pages/RecommendPage.jsx
+++ consoltant/src/pages/RecommendPage.jsx
@@ -4,8 +4,8 @@ import { Routes, Route, Router } from "react-router-dom";
 // 금융 상품 추천 페이지
 // 일단 한 개만 만들어놨는데 추후 금융 상품 추천에 대한 계획이 구체화되면 늘릴 수도 있음
 
-function RecommnedPage() {
+function RecommendPage() {
   return <div></div>;
 }
 
-export default RecommnedPage;
+export default RecommendPage;
