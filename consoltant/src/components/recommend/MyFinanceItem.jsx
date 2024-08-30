8153ebb2b3ae48926508a56fe52d1174dee55e47
고다현 <kdhgby@naver.com>
2024-08-31 08:49:53 +0900

    feat: recommend page

--- consoltant/src/components/recommend/MyFinanceItem.jsx
+++ consoltant/src/components/recommend/MyFinanceItem.jsx
@@ -11,11 +11,14 @@ function MyFinanceItem({ item, onClick, isSelected }) {
     >
       <div>
         <div className="text-base font-semibold">{item.accountName}</div>
-        <p className="text-sm text-[#8E8E93]">{item.accountDescription}</p>
+        <p className="text-base text-[#8E8E93]">{item.accountDescription}</p>
       </div>
       <div className="flex flex-col justify-end items-baseline mt-2">
-        <span className="text-sm mr-2 text-sm">금액 : {item.balance} 원 </span>
-        <span className="text-base text-[#8E8E93]">
+        <span className="text-sm mr-2 text-sm font-OneShinhanMedium">
+          금액 : {item.balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+          원{" "}
+        </span>
+        <span className="text-sm font-OneShinhanMedium ">
           등록기간 : {item.startDate} ~ {item.endDate}
         </span>
       </div>
