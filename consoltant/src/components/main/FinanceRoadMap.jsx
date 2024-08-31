598c13814aefe43ecee418e4aeb660f31f39860d
rlawnsdn <junoo1997@naver.com>
2024-08-31 10:35:32 +0900

    fix navigate to recommend

--- consoltant/src/components/main/FinanceRoadMap.jsx
+++ consoltant/src/components/main/FinanceRoadMap.jsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import ExploreButton from "../common/ExploreButton";
 function FinanceRoadMap() {
+  const navigate = useNavigate();
   return (
     <div className="relative flex flex-col justify-end px-[1rem] pb-[1rem] border bg-[#FAFAFA] rounded-[1rem] h-[13rem] shadow">
       <div className="absolute right-[1rem] top-[1rem]">
-        <ExploreButton />
+        <ExploreButton onClick={() => navigate(`/recommend`)} />
       </div>
       <img
         className="absolute w-[6rem] top-[0.5rem]"
@@ -11,7 +13,8 @@ function FinanceRoadMap() {
         alt=""
       />
       <div className="text-[1.3rem]">
-        유사 성향 <span className="font-OneShinhanBold">금융 로드맵</span>이<br />
+        유사 성향 <span className="font-OneShinhanBold">금융 로드맵</span>이
+        <br />
         도착했어요
       </div>
       <div className="text-[0.8rem]">추천 금융상품</div>
